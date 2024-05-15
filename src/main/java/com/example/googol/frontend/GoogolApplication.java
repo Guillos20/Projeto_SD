8341f998c1cf5ebeedaa0e17f47e701c6f145490
Guillos20 <junqueiraguilherme@hotmail.com>
2024-05-15 13:55:24 +0100

    altered files

--- src/main/java/com/example/googol/frontend/GoogolApplication.java
+++ src/main/java/com/example/googol/frontend/GoogolApplication.java
@@ -1,12 +1,13 @@
 package com.example.googol.frontend;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HackerNetApplication {
+public class GoogolApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HackerNetApplication.class, args);
+		SpringApplication.run(GoogolApplication.class, args);
 	}
 
 }
